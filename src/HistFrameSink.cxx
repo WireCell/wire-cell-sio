df4a0bceb03bbf8bc879c95a58d97f0d8bee8195
Brett Viren <brett.viren@gmail.com>
2017-04-20 12:20:58 -0400

    Fix bugs in finding extent of traces in frames.

--- src/HistFrameSink.cxx
+++ src/HistFrameSink.cxx
@@ -1,11 +1,13 @@
 #include "WireCellSio/HistFrameSink.h"
 #include "WireCellUtil/NamedFactory.h"
 #include "WireCellUtil/BoundingBox.h"
+#include "WireCellUtil/Waveform.h"
 
 #include "TFile.h"
 #include "TH2F.h"
 
 #include <iostream>
+#include <algorithm>
 #include <unordered_map>
 
 WIRECELL_FACTORY(HistFrameSink, WireCell::Sio::HistFrameSink, WireCell::IFrameSink, WireCell::IConfigurable);
@@ -46,48 +48,56 @@ void Sio::HistFrameSink::configure(const WireCell::Configuration& cfg)
 }
 
 
+
 bool Sio::HistFrameSink::operator()(const IFrame::pointer& frame)
 {
     std::string fname = Form(m_filepat.c_str(), frame->ident());
     TFile* file = TFile::Open(fname.c_str(), "recreate");
     
 
-    // fixme: index by packed integer as WirePlaneId can not be hashed.
+    typedef std::tuple< ITrace::vector, std::vector<int>, std::vector<int> > tct_tuple;
+    std::unordered_map<int, tct_tuple> perplane; 
 
-    // plane ident to its traces
-    std::unordered_map<int, ITrace::vector> perplane; 
-
-    // fixme: this is an abuse of bounding box.  A general,
-    // N-dimensional std::tuple based BB should be developed!
-    std::unordered_map<int, BoundingBox> limits;      
-
+    // collate traces into per plane and also calculate bounds 
     ITrace::shared_vector traces = frame->traces();
     for (ITrace::pointer trace : *traces) {
         int ch = trace->channel();
         auto wpident = m_anode->resolve(ch).ident(); 
-        perplane[wpident].push_back(trace);
         double tmin = trace->tbin();
         double tlen = trace->charge().size();
-        limits[wpident](Point(ch, tmin, tmin+tlen));
+
+        auto& tct = perplane[wpident];
+        get<0>(tct).push_back(trace);
+        get<1>(tct).push_back(ch);
+        get<2>(tct).push_back(tmin);
+        get<2>(tct).push_back(tmin+tlen);
     }
 
     const double t0 = frame->time();
     const double tick = frame->tick();
 
 
-    for (auto wpid_bb : limits) {
-        int wpident = wpid_bb.first;
-        auto wpid = WirePlaneId(wpident);
-        Ray bb = wpid_bb.second.bounds();
+    for (auto& thisplane : perplane) {
+        int wpident = thisplane.first;
+        auto& tct = thisplane.second;
+        auto& traces = get<0>(tct);
+        auto& chans = get<1>(tct);
+        auto chmm = std::minmax_element(chans.begin(), chans.end());
+        auto& tbins = get<2>(tct);
+        auto tbmm = std::minmax_element(tbins.begin(), tbins.end());
 
-        // fixme: this is rife for off-by-one bugs
-        const double tmin = t0 + tick*bb.first[1];
-        const double tmax = t0 + tick*bb.second[2];
-        const int ntbins = (tmax-tmin)/tick;
-        const int chmin = round(bb.first[0]);
-        const int chmax = round(bb.first[1]);
-        const int nchbins = chmax - chmin + 1;
+        cerr << wpident
+             << " chan:[" << *chmm.first << "," << *chmm.second << "] "
+             << " tbin:[" << *tbmm.first << "," << *tbmm.second <<"]\n";
 
+
+        const double tmin = t0 + tick*(*tbmm.first);
+        const double tmax = t0 + tick*(*tbmm.second);
+        const int ntbins = (*tbmm.second)-(*tbmm.first);
+
+        const float chmin = *chmm.first;
+        const float chmax = *chmm.second + 1;
+        const int nchbins = (*chmm.second) - (*chmm.first) + 1;
         
         TH2F* hist = new TH2F(Form("plane%d", wpident),
                               Form("Plane %d", wpident),
@@ -95,14 +105,13 @@ bool Sio::HistFrameSink::operator()(const IFrame::pointer& frame)
                               nchbins, chmin, chmax);
         hist->SetDirectory(file); // needed?
 
-        auto& traces = perplane[wpident];
         for (auto& trace : traces) {
             double fch = trace->channel() + 0.5; // make sure we land in bin-center.
             int tbin = trace->tbin();
             auto& charge = trace->charge();
             int nbins = charge.size();
             for (int ibin=0; ibin<nbins; ++ibin) {
-                const double t = t0 + (tick+0.5)*(tbin+ibin); // 0.5 to land in bin-center
+                const double t = t0 + (tick)*(tbin+ibin+0.5); // 0.5 to land in bin-center
                 hist->Fill(t/units::us, fch, charge[ibin]);
             }
         }
