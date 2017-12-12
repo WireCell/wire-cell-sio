c799f5697cfe4d3e468164919683e6a34747b61c
Brett Viren <brett.viren@gmail.com>
2017-12-12 14:14:08 -0500

    Add new interface to also allow saving depos.

--- inc/WireCellSio/NumpySaver.h
+++ inc/WireCellSio/NumpySaver.h
@@ -4,11 +4,13 @@
 #define WIRECELLSIO_NUMPYSAVER
 
 #include "WireCellIface/IFrameFilter.h"
+#include "WireCellIface/IDepoFilter.h"
 #include "WireCellIface/IConfigurable.h"
 
 namespace WireCell {
     namespace Sio {
         class NumpySaver : public WireCell::IFrameFilter,
+                           public WireCell::IDepoFilter,
                            public WireCell::IConfigurable {
         public:
             NumpySaver();
@@ -18,13 +20,20 @@ namespace WireCell {
             virtual bool operator()(const WireCell::IFrame::pointer& inframe,
                                     WireCell::IFrame::pointer& outframe);
 
+            /// IDepoFilter.  This works by buffering depos and saving
+            /// them at the same time a frame is saved.
+            virtual bool operator()(const WireCell::IDepo::pointer& indepo,
+                                    WireCell::IDepo::pointer& outdepo);
+
             /// IConfigurable
             virtual WireCell::Configuration default_configuration() const;
             virtual void configure(const WireCell::Configuration& config);
         private:
 
             Configuration m_cfg;
-            int m_save_count;
+            int m_save_count;   // count frames saved
+
+            std::vector<WireCell::IDepo::pointer> m_depos;
         };
     }
 }
--- src/NumpySaver.cxx
+++ src/NumpySaver.cxx
@@ -8,9 +8,10 @@
 #include <vector>
 #include <algorithm>
 #include <iostream>
+#include <tuple>
 
 WIRECELL_FACTORY(NumpySaver, WireCell::Sio::NumpySaver,
-                 WireCell::IFrameFilter, WireCell::IConfigurable)
+                 WireCell::IFrameFilter, WireCell::IDepoFilter, WireCell::IConfigurable)
 
 using namespace WireCell;
 
@@ -63,6 +64,28 @@ WireCell::Configuration Sio::NumpySaver::default_configuration() const
 void Sio::NumpySaver::configure(const WireCell::Configuration& config)
 {
     m_cfg = config;
+}
+
+
+typedef std::tuple<IDepo::pointer, size_t, size_t> depo_gen_child;
+typedef std::vector< depo_gen_child > depos_with_prior;
+
+static void push_depo(depos_with_prior& dp, WireCell::IDepo::pointer depo, size_t gen=0, size_t childid=0)
+{
+    dp.push_back(depo_gen_child(depo, gen, childid));
+    auto prior = depo->prior();
+    if (!prior) {
+        return;
+    }
+    push_depo(dp, prior, gen+1, dp.size());
+}
+static depos_with_prior flatten_depos(std::vector<WireCell::IDepo::pointer> depos)
+{
+    depos_with_prior ret;
+    for (auto depo : depos) {
+        push_depo(ret, depo);
+    }
+    return ret;
 }
 
 
@@ -133,8 +156,56 @@ bool Sio::NumpySaver::operator()(const IFrame::pointer& inframe,
 
         }
 
+    } // if any frame tags to save
+
+    const size_t ndepos = m_depos.size();
+    if (ndepos) {
+        
+        auto fdepos = flatten_depos(m_depos);
+        const size_t nfdepos = fdepos.size();
+
+        // time, charge, x, y, z, dlong, dtran
+        const size_t ndata=7;
+        Array::array_xxf data(nfdepos, ndata);
+        // ID, pdg, gen, child
+        const size_t ninfo = 4;
+        Array::array_xxi info(nfdepos, ninfo);
+        for (size_t idepo=0; idepo != nfdepos; ++idepo) {
+            auto depogc = fdepos[idepo];
+            auto depo = std::get<0>(depogc);
+            auto gen = std::get<1>(depogc);
+            auto child = std::get<2>(depogc);
+            data(idepo, 0) = depo->time();
+            data(idepo, 1) = depo->charge();
+            data(idepo, 2) = depo->pos().x();
+            data(idepo, 3) = depo->pos().y();
+            data(idepo, 4) = depo->pos().z();
+            data(idepo, 5) = depo->extent_long();
+            data(idepo, 6) = depo->extent_tran();
+            info(idepo, 0) = depo->id();
+            info(idepo, 1) = depo->pdg();
+            info(idepo, 2) = gen;
+            info(idepo, 3) = child;
+        }
+        const std::string data_name = String::format("depo_data_%d", m_save_count);
+        const std::string info_name = String::format("depo_info_%d", m_save_count);
+
+        cnpy::npz_save(fname, data_name, data.data(), {ndata, ndepos}, mode);
+        cnpy::npz_save(fname, info_name, info.data(), {ninfo, ndepos}, mode);
+        m_depos.clear();
     }
+
+
     ++m_save_count;
     return true;
 }
 
+bool Sio::NumpySaver::operator()(const WireCell::IDepo::pointer& indepo,
+                                 WireCell::IDepo::pointer& outdepo)
+{
+    outdepo = indepo;
+    if (indepo) {
+        m_depos.push_back(indepo);
+    }
+    return true;
+}
